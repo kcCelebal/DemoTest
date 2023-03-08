dc8c39f362eeb65572011cd53707d9106cb229b9
kaushik.chowdhury@celebaltech.com <kaushik.chowdhury@celebaltech.com>
2023-03-08 08:59:31 +0000

    updated testing

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
-# creating Spark dataframe
+import json
 
+# COMMAND ----------
 
 # CSV options
 infer_schema = 'true'
@@ -15,3 +16,33 @@ df = spark.read.format(file_type) \
     .option("header", first_row_is_header) \
     .option("sep", delimiter) \
     .load(file_location)
+
+# COMMAND ----------
+
+dic = dict()
+
+dic['columns'] = [{"element_name": 'medallion'},
+                  {'element_name': 'hack_license'},
+                  {'element_name': 'vendor_id'},
+                  {'element_name': 'rate_code_id'},
+                  {'element_name': 'store_and_fwd_flag'},
+                  {'element_name': 'pickup_datetime'},
+                  {'element_name': 'dropoff_datetime'},
+                  {'element_name': 'passenger_count'},
+                  {'element_name': 'trip_time_in_sec'},
+                  {'element_name': 'trip_distance'},
+                  {'element_name': 'pickup_long'},
+                  {'element_name': 'pickup_lat'},
+                  {'element_name': 'dropoff_long'},
+                  {'element_name': 'dropoff_lat'},
+                 ]
+
+
+# COMMAND ----------
+
+view_name = 'sample_file'
+df.createOrReplaceGlobalTempView(view_name)
+
+# COMMAND ----------
+
+dbutils.notebook.exit(json.dumps(dic))
